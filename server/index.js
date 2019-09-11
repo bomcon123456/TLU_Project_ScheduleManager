813fba4946c41dcf468bdd77188291b24e36bf58
Termanteus <bomcon123456@gmail.com>
2019-09-11 08:28:21 +0700

    Initial index

---
+++ server/index.js
@@ -0,0 +1,73 @@
+require("dotenv").config({ path: "./env/dev.env" });
+
+// const path = require("path");
+// const fs = require("fs");
+// const https = require("https");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// const multer = require("multer");
+// const {
+//   fileFilter,
+//   fileStorage
+// } = require("./modules/common/util/multer-util");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Static folder Middleware
+
+// CORS-Middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+// Multer (file upload) middleware
+
+// Routes
+
+// Error-handling Middleware
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data
+  });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(result => {
+    const port = process.env.PORT;
+    console.warn("Listening at port:", port);
+    app.listen(process.env.PORT);
+    /* HTTPs
+    // https
+    //   .createServer(
+    //     {
+    //       key: fs.readFileSync(
+    //         `./modules/common/keys/${process.env.NODE_ENV}/${process.env.SSL_KEY_NAME}`
+    //       ),
+    //       cert: fs.readFileSync(
+    //         `./modules/common/keys/${process.env.NODE_ENV}/${process.env.SSL_CRT_NAME}`
+    //       ),
+    //       requestCert: false,
+    //       rejectUnauthorized: false
+    //     },
+    //     app
+    //   )
+    //   .listen(port);    */
+  })
+  .catch(err => console.log(err));
