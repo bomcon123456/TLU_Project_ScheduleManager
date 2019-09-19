b2b460d00d82ae801b8306271ea04bc844033dc6
Termanteus <bomcon123456@gmail.com>
2019-09-19 13:47:39 +0700

    Department Model

---
+++ server/modules/api/departments/model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const groups = require("../../common/constants/groups");
+const semesters = require("../../common/constants/semesters");
+
+const departmentSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    schoolId: { type: String, required: true, unique: true },
+    requestedCourses: {
+      type: [
+        {
+          group: {
+            type: String,
+            enum: groups,
+            required: true
+          },
+          semester: {
+            type: String,
+            enum: semesters,
+            required: true
+          },
+          courses: {
+            type: [
+              {
+                type: Schema.Types.ObjectId,
+                ref: "Course"
+              }
+            ],
+            default: []
+          }
+        }
+      ],
+      default: [
+        {
+          group: "Group 1",
+          courses: []
+        },
+        {
+          group: "Group 2",
+          courses: []
+        },
+        {
+          group: "Group 3",
+          courses: []
+        }
+      ]
+    }
+  },
+  { timestamps: { createdAt: "createdAt" } }
+);
+
+module.exports = mongoose.model("Department", departmentSchema);
