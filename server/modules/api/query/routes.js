5d94afb376110b8388bdb7e91e10c5ce30a92e09
bomcon123456 <bomcon123456@gmail.com>
2019-10-04 07:47:50 +0700

    Shift/Day Query, Not Test

--- server/modules/api/query/routes.js
+++ server/modules/api/query/routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const Controller = require("./controller");
 
 router.get("/free-rooms", Controller.getFreeRooms);
+router.get("/free-shifts", Controller.getFreeShifts);
+router.get("/free-days", Controller.getFreeDays);
 
 module.exports = router;
